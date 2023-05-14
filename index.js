aea952bd0a8293a45905bf53dd31dd34ae97ff15
asemFoqha <asem.sh.foqha@gmail.com>
2023-05-14 14:52:17 -0500

    fix

--- index.js
+++ index.js
@@ -2,43 +2,46 @@ let gameStarted = false;
 
 let boundary = $(".boundary");
 $(".won").hide();
-$("#start").on("click", () => {
-  gameStarted = true;
-  $("#status").html("Started");
-  boundary.removeClass("lose-status");
-  $(".won").hide();
-});
 
-$(".boundary").on("mouseenter", () => {
-  if (gameStarted) {
-    $("#status").html('Click the "S" to begin.');
-    gameStarted = false;
-    boundary.addClass("lose-status");
+$(document).ready(() => {
+  $("#start").on("click", () => {
+    gameStarted = true;
+    $("#status").html("Started");
+    boundary.removeClass("lose-status");
+    $(".won").hide();
+  });
 
-    setTimeout(() => {
-      alert("You Lost!!");
-    }, 50);
-  }
-});
+  $(".boundary").on("mouseenter", () => {
+    if (gameStarted) {
+      $("#status").html('Click the "S" to begin.');
+      gameStarted = false;
+      boundary.addClass("lose-status");
 
-$("#maze").on("mouseleave", () => {
-  if (gameStarted) {
-    $("#status").html('Click the "S" to begin.');
-    gameStarted = false;
-    boundary.addClass("lose-status");
-    setTimeout(() => {
-      alert("You Lost!!");
-    }, 50);
-  }
-});
+      setTimeout(() => {
+        alert("You Lost!!");
+      }, 50);
+    }
+  });
 
-$("#end").on("mouseenter", () => {
-  if (gameStarted) {
-    $("#status").html('Click the "S" to begin.');
-    gameStarted = false;
-    $(".won").show();
-    setTimeout(() => {
-      alert("You Won");
-    }, 50);
-  }
+  $("#maze").on("mouseleave", () => {
+    if (gameStarted) {
+      $("#status").html('Click the "S" to begin.');
+      gameStarted = false;
+      boundary.addClass("lose-status");
+      setTimeout(() => {
+        alert("You Lost!!");
+      }, 50);
+    }
+  });
+
+  $("#end").on("mouseenter", () => {
+    if (gameStarted) {
+      $("#status").html('Click the "S" to begin.');
+      gameStarted = false;
+      $(".won").show();
+      setTimeout(() => {
+        alert("You Won");
+      }, 50);
+    }
+  });
 });
