c98cf9595d2a4bbf9866c1e3c77ae0a2a29be082
asemFoqha <asem.sh.foqha@gmail.com>
2023-05-14 14:47:50 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+let gameStarted = false;
+
+let boundary = $(".boundary");
+$(".won").hide();
+$("#start").on("click", () => {
+  gameStarted = true;
+  $("#status").html("Started");
+  boundary.removeClass("lose-status");
+  $(".won").hide();
+});
+
+$(".boundary").on("mouseenter", () => {
+  if (gameStarted) {
+    $("#status").html('Click the "S" to begin.');
+    gameStarted = false;
+    boundary.addClass("lose-status");
+
+    setTimeout(() => {
+      alert("You Lost!!");
+    }, 50);
+  }
+});
+
+$("#maze").on("mouseleave", () => {
+  if (gameStarted) {
+    $("#status").html('Click the "S" to begin.');
+    gameStarted = false;
+    boundary.addClass("lose-status");
+    setTimeout(() => {
+      alert("You Lost!!");
+    }, 50);
+  }
+});
+
+$("#end").on("mouseenter", () => {
+  if (gameStarted) {
+    $("#status").html('Click the "S" to begin.');
+    gameStarted = false;
+    $(".won").show();
+    setTimeout(() => {
+      alert("You Won");
+    }, 50);
+  }
+});
